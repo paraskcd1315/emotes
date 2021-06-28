f28e399e26f43bcfa4d85a50bc9a85e8e98ca338
Paras Khanchandani <paras_theloveable@hotmail.com>
2021-06-28 15:10:22 +0530

    Added to automatically resize emotes to 48px

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const emotesFolder = './emotes/';
 const fs = require('fs');
+const resizeImg = require('resize-img');
+const gifResize = require('@gumlet/gif-resize');
 const { name, pathName } = require('./repoData');
 
 let emotesFileData = {
@@ -10,17 +12,44 @@ let emotesFileData = {
 
 fs.readdir(emotesFolder, (err, files) => {
 	if (err) console.error(err.message);
-	files.forEach((file) => {
+
+	files.forEach(async (file) => {
 		let emote = {
 			name: file.split('.')[0],
 			type: '.' + file.split('.')[1]
 		};
+
 		emotesFileData.emotes.push(emote);
+
+		if (file.split('.')[1] === 'png') {
+			try {
+				const image = await resizeImg(fs.readFileSync(emotesFolder + file), {
+					width: 48
+				});
+
+				fs.writeFileSync(emotesFolder + file, image);
+				console.log(file + ' PNG Image Resized');
+			} catch (err) {
+				console.error(err.message);
+			}
+		} else {
+			try {
+				gifResize({
+					width: 48
+				})(fs.readFileSync(emotesFolder + file)).then((data) => {
+					fs.writeFileSync(emotesFolder + file, data);
+					console.log(file + ' GIF Image Resized');
+				});
+			} catch (error) {
+				console.error(err.message);
+			}
+		}
 	});
+
 	fs.writeFile('index.json', JSON.stringify(emotesFileData), 'utf8', (err) => {
 		if (err) {
 			console.error(err);
 		}
-		console.log('index.json was made');
+		console.log('\nindex.json was made');
 	});
 });
